ccc2bf59d436ce56c2d240530304094e685946cc
Christopher Seven Phiri <chris7phiri@gmail.com>
2024-10-29 23:24:12 +0200

    Init react charts

--- src/config/services.ts
+++ src/config/services.ts
@@ -1,14 +1,19 @@
 import { Service } from "@/lib/types";
 
 export const allServices: Service[] = [
-    {
-      title: "Duty Import Calculator",
-      description: "Calculate the cost of importing a car in Malawi",
-      url: "/mra/duty-calculator",
-    },
-    {
-      title: "Malawi POSTCODES",
-      description: "All post codes for malawi",
-      url: "/macra/postcodes",
-    },
-  ];
+  {
+    title: "Duty Import Calculator",
+    description: "Calculate the cost of importing a car in Malawi",
+    url: "/mra/duty-calculator",
+  },
+  {
+    title: "Malawi POSTCODES",
+    description: "All post codes for malawi",
+    url: "/macra/postcodes",
+  },
+  {
+    title: "Malawi Inflation Rate",
+    description: "Get the latest inflation rate in Malawi",
+    url: "/rbm/inflation-rate",
+  },
+];
